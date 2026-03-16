ca827b990a8c0dc577671cda9748339339c3b488
R4mFaaaw <rafamirzafirdaus@gmail.com>
2026-03-16 20:32:38 +0700

    tinggal ngoding

--- uts.cpp
+++ uts.cpp
@@ -4,8 +4,41 @@
 
 using namespace std;
 
-// --- STRUKTUR DATA ---
+// --- STRUKTUR DATA --- well
 struct Barang {
     int id;
     string nama;
 };
+
+// Single Linked List untuk Antrean ya gaes ya
+struct NodeAntrean {
+    Barang data;
+    NodeAntrean* next;
+};
+
+// Double Linked List untuk Riwayat ya gaes ya
+struct NodeLog {
+    string aktivitas;
+    NodeLog* next;
+    NodeLog* prev;
+};
+
+// ngoding well #MALES
+void tambahAntrean(){}
+void prosesAntrean(){}
+void tampilkanSemuaBarang(){}
+void cariBarang(){}
+void lihatRiwayat(){}
+void tambahlog(){}
+
+int main(){
+    cout << "1. Tambah barang" << endl;
+    cout << "2. Hapus barang" << endl;
+    cout << "3. Tampilkan semua barang" << endl;
+    cout << "4. Cari barang" << endl;
+    cout << "5. Riwayat barang" << endl;
+    cout << "6. Keluar" << endl;
+    cout << "Pilihan: ";
+
+    return 0;
+}
